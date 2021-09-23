b52aa2b019911db0ffa7b721ab1f8a9376ef1819
Jeff Palm <jeff@jeffpalm.com>
2021-09-23 05:57:51 -0400

    net

---
+++ net/net.go
@@ -0,0 +1,29 @@
+package net
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+// https://golangcode.com/download-a-file-from-a-url/
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
