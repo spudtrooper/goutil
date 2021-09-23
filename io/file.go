f3e7e6b30819fb13956875f6122c6e40c91547ea
Jeff Palm <jeff@jeffpalm.com>
2021-09-23 05:54:22 -0400

    mkdirall

--- io/file.go
+++ io/file.go
@@ -1,10 +1,21 @@
 package io
 
-import "os"
+import (
+	"os"
+	"path"
+)
 
 func FileExists(f string) bool {
 	if _, err := os.Stat(f); os.IsNotExist(err) {
 		return false
 	}
 	return true
+}
+
+func MkdirAll(paths ...string) (string, error) {
+	outDir := path.Join(paths...)
+	if err := os.MkdirAll(outDir, 0755); err != nil {
+		return "", err
+	}
+	return outDir, nil
 }
