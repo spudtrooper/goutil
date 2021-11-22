8fd98850c980781869955599169b793e3abf945f
Jeff Palm <jeff@jeffpalm.com>
2021-11-22 09:11:56 -0500

    update Mon Nov 22 09:11:56 EST 2021

---
+++ html/linkify.go
@@ -0,0 +1,13 @@
+package html
+
+import "regexp"
+
+var (
+	urlRE = regexp.MustCompile(`(https?://)([\.a-zA-Z_0-9/\-]+)`)
+)
+
+func Linkify(text string) string {
+	html := text
+	html = urlRE.ReplaceAllString(html, `<a target="_" href="$1$2">$2</a>`)
+	return html
+}
