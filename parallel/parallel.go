92e8e3423ba08cad926d125360c01bd89f30bf36
Jeff Palm <jeff@jeffpalm.com>
2022-01-31 22:37:25 -0500

    update Mon Jan 31 22:37:25 EST 2022

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -10,6 +10,18 @@ func ExecAndDrain(collection chan interface{}, threads int, fn func(interface{})
 	EmptyDrain(res, errs)
 }
 
+func DoTimes(threads int, fn func()) {
+	var wg sync.WaitGroup
+	for i := 0; i < threads; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fn()
+		}()
+	}
+	wg.Wait()
+}
+
 func Exec(collection chan interface{}, threads int, fn func(interface{}) (interface{}, error)) (chan interface{}, chan error) {
 	results := make(chan interface{})
 	errors := make(chan error)
