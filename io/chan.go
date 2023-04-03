d0cedd2db9b4fb952d9f0e6a7d9172781fa09ffe
Jeff Palm <jeff@jeffpalm.com>
2023-04-03 08:20:00 -0400

    update Mon Apr  3 08:20:00 EDT 2023

--- io/chan.go
+++ io/chan.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"sync"
 )
 
 func StringsFromFile(filepath string, sOpts ...StringsFromFileOption) (chan string, error) {
@@ -32,4 +33,54 @@ func StringsFromFile(filepath string, sOpts ...StringsFromFileOption) (chan stri
 	}()
 
 	return res, nil
+}
+
+func StringsFromFiles(filepaths []string, sOpts ...StringsFromFileOption) (chan string, chan error, error) {
+	opts := MakeStringsFromFileOptions(sOpts...)
+
+	res := make(chan string)
+	errs := make(chan error)
+
+	fs := make(chan string)
+	go func() {
+		for _, f := range filepaths {
+			fs <- f
+		}
+		close(fs)
+	}()
+
+	go func() {
+		var wg sync.WaitGroup
+		for filepath := range fs {
+
+			f, err := os.Open(filepath)
+			if err != nil {
+				errs <- err
+				continue
+			}
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				defer f.Close()
+				scanner := bufio.NewScanner(f)
+				for scanner.Scan() {
+					s := scanner.Text()
+					if opts.SkipEmpty() && s == "" {
+						continue
+					}
+					if opts.CommentStart() != "" && strings.HasPrefix(s, opts.CommentStart()) {
+						continue
+					}
+					res <- s
+				}
+			}()
+		}
+
+		wg.Wait()
+		close(res)
+		close(errs)
+	}()
+
+	return res, errs, nil
 }
