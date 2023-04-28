0ef51dc8cd0e875accbfb8bafde9768bf01f6f71
Jeff Palm <jeff@jeffpalm.com>
2023-04-28 05:48:31 -0400

    update Fri Apr 28 05:48:31 EDT 2023

--- slice/slice.go
+++ slice/slice.go
@@ -1,52 +1,73 @@
-// DO NOT EDIT MANUALLY: Generated from https://github.com/spudtrooper/genopts
 package slice
 
-import "fmt"
+import (
+	"strconv"
+	"strings"
 
-type StringsOption struct {
-	f func(*stringsOptionImpl)
-	s string
-}
+	"github.com/pkg/errors"
+	"github.com/spudtrooper/goutil/or"
+	"github.com/spudtrooper/goutil/sets"
+)
 
-func (o StringsOption) String() string { return o.s }
-
-type StringsOptions interface {
-	TrimSpace() bool
-	HasTrimSpace() bool
-}
-
-func StringsTrimSpace(trimSpace bool) StringsOption {
-	return StringsOption{func(opts *stringsOptionImpl) {
-		opts.has_trimSpace = true
-		opts.trimSpace = trimSpace
-	}, fmt.Sprintf("slice.StringsTrimSpace(bool %+v)", trimSpace)}
-}
-func StringsTrimSpaceFlag(trimSpace *bool) StringsOption {
-	return StringsOption{func(opts *stringsOptionImpl) {
-		if trimSpace == nil {
-			return
+//go:generate genopts --function Strings trimSpace
+func Strings(input, sep string, sOpts ...StringsOption) []string {
+	opts := MakeStringsOptions(sOpts...)
+	if input == "" {
+		return []string{}
+	}
+	var res []string
+	for _, s := range strings.Split(input, sep) {
+		if opts.TrimSpace() {
+			s = strings.TrimSpace(s)
 		}
-		opts.has_trimSpace = true
-		opts.trimSpace = *trimSpace
-	}, fmt.Sprintf("slice.StringsTrimSpace(bool %+v)", trimSpace)}
-}
-
-type stringsOptionImpl struct {
-	trimSpace     bool
-	has_trimSpace bool
-}
-
-func (s *stringsOptionImpl) TrimSpace() bool    { return s.trimSpace }
-func (s *stringsOptionImpl) HasTrimSpace() bool { return s.has_trimSpace }
-
-func makeStringsOptionImpl(opts ...StringsOption) *stringsOptionImpl {
-	res := &stringsOptionImpl{}
-	for _, opt := range opts {
-		opt.f(res)
+		res = append(res, s)
 	}
 	return res
 }
 
-func MakeStringsOptions(opts ...StringsOption) StringsOptions {
-	return makeStringsOptionImpl(opts...)
+// StringDiff returns an array with all elements in `a` not in `b`.
+func StringDiff(a, b []string) []string {
+	res := []string{}
+	bSet := sets.String(b)
+	for _, x := range a {
+		if !bSet[x] {
+			res = append(res, x)
+		}
+	}
+	return res
+}
+
+func Reverse(ss []string) {
+	last := len(ss) - 1
+	for i := 0; i < len(ss)/2; i++ {
+		ss[i], ss[last-i] = ss[last-i], ss[i]
+	}
+}
+
+func NonEmptyStrings(ss []string) []string {
+	res := []string{}
+	for _, s := range ss {
+		if s != "" {
+			res = append(res, s)
+		}
+	}
+	return res
+}
+
+//go:generate genopts --function Ints sep:string trimSpace
+func Ints(s string, optss ...IntsOption) ([]int, error) {
+	opts := MakeIntsOptions(optss...)
+	sep := or.String(opts.Sep(), ",")
+	res := []int{}
+	for _, s := range NonEmptyStrings(strings.Split(s, sep)) {
+		if opts.TrimSpace() {
+			s = strings.TrimSpace(s)
+		}
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return nil, errors.Errorf("strconv.Atoi(%q): %v", s, err)
+		}
+		res = append(res, i)
+	}
+	return res, nil
 }
